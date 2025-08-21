418b4ae949f21d7bcde390fdffe61c2b64c34c80
pmikszta <p_mikszta@mail.harpercollege.edu>
2025-08-20 23:18:41 -0500

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,8 +1,6 @@
-package org.example;
-
-public class App {
+//super simple simple java Hello world script 
+public class HelloWorld {
     public static void main(String[] args) {
-      Greeter greeter = new Greeter();
-      System.out.println(greeter.getGreeting());
+        System.out.println("Hello, World! Java Edition");
     }
 }
