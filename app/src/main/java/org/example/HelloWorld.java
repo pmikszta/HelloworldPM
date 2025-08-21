e2652d5a0dc7870de8d84a6e88b60b33befadf5a
pmikszta <p_mikszta@mail.harpercollege.edu>
2025-08-20 23:17:41 -0500

    Add files via upload

---
+++ app/src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,6 @@
+//super simple simple java Hello world script 
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! Java Edition");
+    }
+}
