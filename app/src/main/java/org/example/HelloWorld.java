1f294ce2ecfb0a762cbb1b10c5f0fe62ed797d45
Patrick Mikszta <p_mikszta@mail.harpercollege.edu>
2025-08-21 12:57:56 -0500

    delete

--- app/src/main/java/org/example/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-//super simple simple java Hello world script 
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World! Java Edition");
-    }
-}
